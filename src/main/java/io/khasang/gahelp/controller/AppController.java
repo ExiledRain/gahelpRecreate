506e168674366eeac3621228f4fead4b97cfde56
ExiledRain <gett_lostt@bk.ru>
2020-12-27 16:34:40 +0200

    Initial project commit

---
+++ src/main/java/io/khasang/gahelp/controller/AppController.java
@@ -0,0 +1,21 @@
+package io.khasang.gahelp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+
+    @RequestMapping("/")
+    public String getStatus() {
+        return "status";
+    }
+
+    @RequestMapping("/hello/{name}")
+    public String getHelloPage(@PathVariable("name")String name, Model model){
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
